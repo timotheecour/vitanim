666e176a09df70db5381a186afeced66ff6514c4
Timothee Cour <timothee.cour2@gmail.com>
2019-01-20 04:41:53 -0800

    _

--- ci_setup.py
+++ ci_setup.py
@@ -1,17 +1,18 @@
 import os
 import shutil
 
-print("python start1")
+print("python script v2")
 
 def runCmd(cmd):
   print(("runCmd",cmd))
   import subprocess
-  # subprocess.check_call(cmd, shell=True) # BUG: this doesn't print anything in azure!
-  output = subprocess.check_output(cmd, shell=True)
-  output=output.decode('utf8')
-  print("output:")
-  print(output)
+  subprocess.check_call(cmd, shell=True) # BUG: this doesn't print anything in azure!
   # subprocess.check_call(cmd, shell=True, stdout=subprocess.STDOUT)
+  if False:
+    output = subprocess.check_output(cmd, shell=True)
+    output=output.decode('utf8')
+    print("output:")
+    print(output)
 
 def buildNimCsources():
   print("in buildNimCsources")
