c88b7ce5c609a0367e7d2366520ca938fecc630e
Timothee Cour <timothee.cour2@gmail.com>
2019-01-20 07:04:20 -0800

    _

--- ci_setup.py
+++ ci_setup.py
@@ -4,14 +4,13 @@ import shutil
 print("python script v2")
 
 def runCmd(cmd):
+  # D20190120T053813:here
   print(("runCmd",cmd))
   import subprocess
-  subprocess.check_call(cmd, shell=True) # BUG: this doesn't print anything in azure!
-  # subprocess.check_call(cmd, shell=True, stdout=subprocess.STDOUT)
+  subprocess.check_call(cmd, shell=True) # works (w unbuffer)
   if False:
     output = subprocess.check_output(cmd, shell=True)
     output=output.decode('utf8')
-    print("output:")
     print(output)
 
 def buildNimCsources():
