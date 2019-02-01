2d37d427557316e5f408165a1765841ea11a813b
Timothee Cour <timothee.cour2@gmail.com>
2019-02-01 01:58:19 -0800

    DSLtoLLVMIR: nim code to llvm IR

--- testcases/tests/t0127.nim
+++ testcases/tests/t0127.nim
@@ -1,41 +1,66 @@
 #[
-KEY llvm IR
+KEY llvm IR Halide
+D20190201T004613
 
-output:
-
-; ModuleID = '/tmp/foo.c'
-source_filename = "/tmp/foo.c"
-target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
-target triple = "x86_64-apple-macosx10.14.0"
-
-; Function Attrs: noinline nounwind optnone ssp uwtable
-define void @main() #0 {
-  ret void
-}
-
-attributes #0 = { noinline nounwind optnone ssp uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="penryn" "target-features"="+cx16,+fxsr,+mmx,+sahf,+sse,+sse2,+sse3,+sse4.1,+ssse3,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
-
-!llvm.module.flags = !{!0, !1}
-!llvm.ident = !{!2}
-
-!0 = !{i32 1, !"wchar_size", i32 4}
-!1 = !{i32 7, !"PIC Level", i32 2}
-!2 = !{!"Apple LLVM version 10.0.0 (clang-1000.11.45.5)"}
-
+output (snippet):
+```
+define void @entryPoint() #0 {
+  %1 = alloca %struct.TFrame_, align 8
+  %2 = alloca i64, align 8
+  %3 = alloca i64, align 8
+  %4 = alloca [1 x %struct.NimStringDesc*], align 8
+  %5 = alloca i64, align 8
+  %6 = alloca i64, align 8
+  %7 = getelementptr inbounds %struct.TFrame_, %struct.TFrame_* %1, i32 0, i32 1
+...
+```
 
 ]#
 
-import strformat
+import strformat, macros, os
 
-proc generateLLVMIR(input: string): string =
-  let file = "/tmp/foo.c"
+# IMPROVE
+from "$nim/testament/lib/stdtest/specialpaths.nim" import stdlibDir
+
+proc generateLLVMIR_file(file: string): string =
   let outFile = "/tmp/foo.ll"
-  writeFile file, input
-  let (output, exitCode) = gorgeEx(&"clang -S -emit-llvm -o {outFile} {file}")
+  let cmd = &"clang -S -emit-llvm -o {outFile} -I {stdlibDir} {file}"
+  echo cmd
+  let (output, exitCode) = gorgeEx cmd
   doAssert exitCode == 0
   return outFile.readFile
 
-static:
-  echo generateLLVMIR """
-  void main(){}
-"""
+proc generateLLVMIR_code(input: string): string =
+  let file = "/tmp/foo.c"
+  writeFile file, input
+  generateLLVMIR_file(file)
+
+proc DSLtoLLVMIRImpl(code: string): string =
+  let outDir = "/tmp/d07"
+  let fileSrc = outDir / "foo2.nim"
+  fileSrc.writeFile code
+  let fileOut = fileSrc.changeFileExt(".c")
+
+  let cmd = &"nim c --noMain --compileOnly --nimcache:{outDir} -o:{fileOut} {fileSrc}"
+  echo cmd
+  let (output, exitCode) = gorgeEx(cmd)
+  echo output
+  doAssert exitCode == 0
+  echo fileOut
+  result = generateLLVMIR_file(fileOut)
+
+macro DSLtoLLVMIR(body: untyped): untyped =
+  #[
+  # note: could have a preprocessing macro to interpret a DSL, eg:
+  forEach x in a, y in b, z in c:
+    z = x + y
+  ]#
+  let code = body.repr
+  let ir = DSLtoLLVMIRImpl(code)
+  result = quote do:
+    echo `ir`
+
+DSLtoLLVMIR:
+  proc entryPoint() {.exportc.} =
+    for i in 0..<10:
+      echo i*i
