5e458688cecd02e04cd5f7b945ce9ce0cd7a4826
Timothee Cour <timothee.cour2@gmail.com>
2019-02-01 00:41:00 -0800

    _

--- testcases/tests/t0127.nim
+++ testcases/tests/t0127.nim
@@ -5,10 +5,9 @@ UNTESTED
 ]#
 
 proc generateLLVMIR(input: sring): string =
-  let code = body.repr
   let file = "/tmp/foo.c"
   let outFile = "/tmp/foo.ll"
-  writeFile file, code
+  writeFile file, input
   let (output, exitCode) = gorgeEx(&"clang -S -emit-llvm -o {outFile} {file}")
   doAssert exitCode == 0
   return outFile.readFile
