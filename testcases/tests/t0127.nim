4e0e5ae8c17a02cbf3290980bc5d2c1dff578b61
Timothee Cour <timothee.cour2@gmail.com>
2019-02-01 00:42:47 -0800

    fixup

--- testcases/tests/t0127.nim
+++ testcases/tests/t0127.nim
@@ -1,10 +1,33 @@
 #[
 KEY llvm IR
 
-UNTESTED
+output:
+
+; ModuleID = '/tmp/foo.c'
+source_filename = "/tmp/foo.c"
+target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx10.14.0"
+
+; Function Attrs: noinline nounwind optnone ssp uwtable
+define void @main() #0 {
+  ret void
+}
+
+attributes #0 = { noinline nounwind optnone ssp uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="penryn" "target-features"="+cx16,+fxsr,+mmx,+sahf,+sse,+sse2,+sse3,+sse4.1,+ssse3,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.module.flags = !{!0, !1}
+!llvm.ident = !{!2}
+
+!0 = !{i32 1, !"wchar_size", i32 4}
+!1 = !{i32 7, !"PIC Level", i32 2}
+!2 = !{!"Apple LLVM version 10.0.0 (clang-1000.11.45.5)"}
+
+
 ]#
 
-proc generateLLVMIR(input: sring): string =
+import strformat
+
+proc generateLLVMIR(input: string): string =
   let file = "/tmp/foo.c"
   let outFile = "/tmp/foo.ll"
   writeFile file, input
@@ -12,6 +35,7 @@ proc generateLLVMIR(input: sring): string =
   doAssert exitCode == 0
   return outFile.readFile
 
-static: generateLLVMIR: """
+static:
+  echo generateLLVMIR """
   void main(){}
 """
