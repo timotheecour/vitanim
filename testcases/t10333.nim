88d5cffce2d4d3750529f0ee4cea1a787e398b38
Timothee Cour <timothee.cour2@gmail.com>
2020-03-11 05:05:09 -0700

    _

--- testcases/t10333.nim
+++ testcases/t10333.nim
@@ -35,3 +35,38 @@ when defined case1:
   # test(cpuTime)
   test(getMonoTime)
   test(getTicks)
+
+when defined case2:
+  # include system/timers
+  # import t10333b
+  import std/timers
+  import std/monotimes
+  import times
+  import strutils
+  proc toSeconds(a: Nanos): float = a.float*1e-9
+  proc toSeconds(a: float): float = a
+  proc toSeconds(a: Duration): float = a.nanoseconds.float*1e-9
+
+
+  template test(fun)=
+    block:
+      proc main()=
+        let n = 10_000_000
+        type T = type(fun()-fun())
+        var dt: T
+        for i in 0..<n:
+          let t = fun()
+          # code to benchmark here (intentionally empty)
+          let t2 = fun()
+          dt += t2-t
+        echo "\n" & astToStr(fun) & ":"
+        echo ("total secs", dt.toSeconds)
+        echo ("ns/iter", (dt.toSeconds * 1e9 / n.float).formatEng)
+        echo ("iters per sec", (n.float / dt.toSeconds).formatEng)
+      let tot = cpuTime()
+      main()
+      echo ("tot: ", cpuTime() - tot)
+
+  test(cpuTime)
+  test(getMonoTime)
+  test(getTicks)
