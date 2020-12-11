4b7a34ff0b969a05ef38b1603356f7da3d92b4d0
Timothee Cour <timothee.cour2@gmail.com>
2020-12-11 00:20:22 -0800

    add benchmark for https://github.com/nim-lang/Nim/pull/16198

---
+++ src/vitanim/once1.nim
@@ -0,0 +1,42 @@
+#[
+from https://github.com/nim-lang/Nim/pull/16192/files
+]#
+
+import std/[atomics, locks]
+
+type
+  Once* = object    ## A object that ensures a block of code is executed once.
+    finished: Atomic[bool]
+    lock: Lock
+
+proc initOnce*(once: var Once) =
+  ## Initializes a `Once` object.
+  once.finished.store(false)
+  initLock(once.lock)
+
+template once*(cond: Once, body: untyped) =
+  ## Executes a block of code only once (the first time the block is reached).
+  ## It is thread-safe.
+  runnableExamples:
+    var block1: Once
+    var count = 0
+    initOnce(block1)
+
+    for i in 1 .. 10:
+      once(block1):
+        inc count
+
+      # only the first `block1` is executed
+      once(block1):
+        count = 888
+
+    doAssert count == 1
+
+  if not cond.finished.load(moAcquire):
+    withLock cond.lock:
+      # TODO load a value without atomic
+      if not cond.finished.load(moRelaxed):
+        try:
+          body
+        finally:
+          cond.finished.store(true, moRelease)
---
+++ testcases/t10976.nim
@@ -0,0 +1,45 @@
+#[
+KEY xyz
+]#
+
+when true:
+  # benchmark
+  import std/times
+  import std/threadutils # https://github.com/nim-lang/Nim/pull/16198
+  # import timn/exp/once1
+  import vitanim/once1
+
+  var block1: Once
+  initOnce(block1)
+
+  template mainAux(algo) =
+    block:
+      proc bar(i: int, c: int): int =
+        result = i
+        template fn =
+          echo (i, c)
+          result += c
+        if c >= 1234:
+          when algo == "once(block1)":
+            once(block1): fn()
+          elif algo == "onceGlobal":
+            onceGlobal: fn()
+          elif algo == "onceThread":
+            onceThread: fn()
+          elif algo == "system.once":
+            system.once: fn()
+          else: static: doAssert false
+      proc main =
+        let n = 500_000_000
+        var c = 0
+        let t = cpuTime()
+        for i in 0..<n:
+          c+=bar(i, c)
+        let t2 = cpuTime() - t
+        echo (c, astToStr(algo), t2)
+      for j in 0..<5:
+        main()
+  mainAux("once(block1)")
+  mainAux("onceGlobal")
+  mainAux("onceThread")
+  mainAux("system.once")
