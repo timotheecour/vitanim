f339dd29454969fe65b24907035e6b75d71244a3
Timothee Cour <timothee.cour2@gmail.com>
2021-03-04 18:40:30 -0800

    _

--- testcases/tests/t10468b.nim
+++ testcases/tests/t10468b.nim
@@ -18,3 +18,21 @@ $`strutils.delete`
 `strutils.delete`
 
 ]##
+
+##[
+see :`strutils.delete`
+
+see :`delete`
+
+see :`delete`, :`strutils.delete`
+]##
+
+##[
+see `strutils.delete`_
+
+see `strutils.delete`_, `strutils.delete`_.
+
+see `strutils`_, `strutils`_.
+
+see `strutils`_, `strutils`_, foo, `foo`, `bar`_.
+]##
--- testcases/tests/test.md
+++ testcases/tests/test.md
@@ -6,3 +6,7 @@ PRTEMP2
 $`strutils.delete`
 
 `strutils.delete`
+
+see also: `strutils.delete`_.
+
+see :`strutils.delete`
